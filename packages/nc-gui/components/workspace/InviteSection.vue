14bc9ea3cd1a77d5e409c0c9b719bcf9f8237157
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-09-22 18:56:26 +0000

    feat: email badge on enter

--- packages/nc-gui/components/workspace/InviteSection.vue
+++ packages/nc-gui/components/workspace/InviteSection.vue
@@ -101,11 +101,12 @@ const inviteCollaborator = async () => {
     message.error(await extractSdkResponseErrorMsg(e))
   }
 }
-
+// all user input emails are stored here
+const emailBadges = ref<Array<string>>([])
 watch(inviteData, (newVal) => {
-  console.log(newVal.email)
-  if (newVal.email[-1] === ',') {
-    console.log(newVal.email)
+  if (newVal.email.includes(',')) {
+    emailBadges.value.push(newVal.email.split(',')[0])
+    inviteData.email = ''
   }
 })
 
@@ -169,18 +170,38 @@ const onPaste = (e: ClipboardEvent) => {
 
 <template>
   <div class="my-2 pt-3 ml-2" data-testid="invite">
-    <div class="flex gap-2">
-      <div class="flex flex-col">
-        <div
-          ref="divRef"
-          class="flex w-130 border-1 gap-1 items-center flex-wrap min-h-8 max-h-30 rounded-lg nc-scrollbar-md"
-          tabindex="0"
-          :class="{
-            'border-primary/100': isDivFocused,
-            'p-1': emailBadges.length > 1,
-          }"
-          @click="focusOnDiv"
-          @blur="isDivFocused = false"
+    <div class="text-xl mb-4">Invite</div>
+    <a-form>
+      <div class="flex gap-2">
+        <span
+          v-for="(email, index) in emailBadges"
+          :key="email"
+          class="p-1 border-1 border-grey rounded-2xl flex items-center justify-between"
+        >
+          {{ email }}
+          <component :is="iconMap.close" class="ml-1.5 hover:cursor-pointer" @click="emailBadges.splice(index, 1)" />
+        </span>
+        <a-input
+          id="email"
+          v-model:value="inviteData.email"
+          placeholder="Enter emails to send invitation"
+          class="!max-w-130 !rounded"
+          @press-enter="inviteData.email += ','"
+        />
+
+        <RolesSelector
+          class="px-1"
+          :role="inviteData.roles"
+          :roles="allowedRoles"
+          :on-role-change="(role: WorkspaceUserRoles) => (inviteData.roles = role)"
+          :description="true"
+        />
+
+        <a-button
+          type="primary"
+          class="!rounded-md"
+          :disabled="!inviteData.email?.length || isInvitingCollaborators"
+          @click="inviteCollaborator"
         >
           <div class="flex flex-row items-center gap-x-2 pr-1">
             <GeneralLoader v-if="isInvitingCollaborators" class="flex" />
